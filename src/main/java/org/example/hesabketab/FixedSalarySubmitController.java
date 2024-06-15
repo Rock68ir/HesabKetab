d659b37635b81ef2e7891cd121ac65493ea70646
Abolfazl <rock68ir@gmail.com>
2024-06-15 13:06:24 +0430

    Add Abstract base class for controllers and rework PH , Fix controller

--- src/main/java/org/example/hesabketab/FixedSalarySubmitController.java
+++ src/main/java/org/example/hesabketab/FixedSalarySubmitController.java
@@ -1,11 +1,6 @@
 package org.example.hesabketab;
 
-import io.github.palexdev.materialfx.controls.MFXCheckbox;
-import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.event.ActionEvent;
-import org.example.hesabketab.Status.GENDER;
-
-import java.time.LocalDate;
 
 public class FixedSalarySubmitController extends SubmitSalaryBaseController{
     @Override
